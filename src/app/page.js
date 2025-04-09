a43d4f49a166fd2540905c2aae1b4946818fe1e7
Abdullah Rasheed <abdullahrasheed837@gmail.com>
2025-04-09 23:41:23 +0300

    mini change

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-6 bg-gray-100">
     <h1 className="text-3xl font-bold mb-6 text-gray-900">
-      Universal Unit Converter
+      Universal Unit Converter.....
     </h1>
 
       <Converter />
